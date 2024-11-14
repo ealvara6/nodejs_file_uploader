fb52cc12518e82c8343625f8d47a445d69efbb23
Eduardo Alvarado <ealvara73@gmail.com>
2024-11-13 18:19:24 -0600

    Add tailwind configuration and create simple home page

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const sessionConfig = require('./sessionConfig');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
+const routes = require('./routes/index');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(sessionConfig);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use('/', routes);
 
 const PORT = process.env.PORT || 3000;
 
